7f7225c5756bf32df8ad1f47b64c16565c56eb79
kkxlkkxllb@gmail.com <kkxlkkxllb@gmail.com>
2012-12-19 18:06:42 +0800

    annotate

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: courses
+#
+#  id         :integer          not null, primary key
+#  title      :string(255)
+#  ctags      :string(255)
+#  language   :string(255)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Course < ActiveRecord::Base
   attr_accessible :ctags, :language, :title
 end
