8885b0a50508ca69562c25acc43d47af1662fef6
kkxlkkxllb@gmail.com <kkxlkkxllb@gmail.com>
2012-12-21 10:15:47 +0800

    add info

--- README.md
+++ README.md
@@ -2,3 +2,11 @@
 ---------------
 
 > A city of wisdom
+
+# Project Power Support Need
+
+> Server: ubuntu on AWS EC2 (1+ year)
+
+> Front-end Web Engineer: html5 & css3 & UI Design -> Co-Founder (1)
+
+> Educator: Sociology & History & Literature -> Co-Founder (1)
--- db/schema.rb
+++ db/schema.rb
@@ -35,22 +35,24 @@ ActiveRecord::Schema.define(:version => 20121220143535) do
   end
 
   create_table "members", :force => true do |t|
-    t.string   "email"
-    t.string   "encrypted_password"
+    t.string   "email",                  :default => "", :null => false
+    t.string   "encrypted_password",     :default => "", :null => false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count"
+    t.integer  "sign_in_count",          :default => 0
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",                             :null => false
+    t.datetime "updated_at",                             :null => false
     t.string   "role"
     t.string   "uid"
   end
 
+  add_index "members", ["email"], :name => "index_members_on_email", :unique => true
+  add_index "members", ["reset_password_token"], :name => "index_members_on_reset_password_token", :unique => true
   add_index "members", ["role", "uid"], :name => "index_members_on_role_and_uid", :unique => true
 
   create_table "providers", :force => true do |t|
